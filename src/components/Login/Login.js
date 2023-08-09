aed81e58e55a075a0f85703505d33c3c1bd69585
harshita sethiya <harshita.sethiya252@gmail.com>
2023-08-10 01:46:51 +0530

    commit2

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect ,useReducer} from 'react';
-
+import React, { useState, useEffect ,useReducer,useContext} from 'react';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer=(state,action)=>{
   if(action.type==='USER_INPUT'){
@@ -16,18 +16,34 @@ const emailReducer=(state,action)=>{
   }
   return{value:'',isValid:false};
 };
+const passwordReducer=(state,action)=>{
+  if(action.type==='USER_INPUT'){
+    return{value:action.val,isValid:action.val.includes('@')};
+  
 
+  }
+  if(action.type==='INPUT_BLUR'){
+    return{value:state.value,isValid:state.value.includes('@')};
+
+  }
+  return{value:'',isValid:false};
+};
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
-  const [formIsValid, setFormIsValid] = useState(false);
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
+   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState,dispatchEmail]=useReducer(emailReducer,{
     value:'', 
     isValid:null,
   });
+  const [passwordState,dispatchPassword]=useReducer(passwordReducer,{
+    value:'', 
+    isValid:null,
+  });
+  const authCtx = useContext(AuthContext);
    
   useEffect(() => {
   
@@ -37,48 +53,50 @@ const Login = (props) => {
       console.log('EFFECT CLEANUP');
     };
   }, []);
+   const {isValid:emailIsValid}=emailState;
+   const {isValid:passwordIsValid}=passwordStore;
+  
+   useEffect(() => {
+     const identifier = setTimeout(() => {
+       console.log('Checking form validity!');
+       setFormIsValid(
+         enteredEmail.includes('@') && enteredPassword.trim().length > 6
+       );
+     }, 500);
 
-  // useEffect(() => {
-  //   const identifier = setTimeout(() => {
-  //     console.log('Checking form validity!');
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
-  //   }, 500);
-
-  //   return () => {
-  //     console.log('CLEANUP');
-  //     clearTimeout(identifier);
-  //   };
-  // }, [enteredEmail, enteredPassword]);
+    return () => {
+      console.log('CLEANUP');
+       clearTimeout(identifier);
+     };
+   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
    dispatchEmail({type:'USER_INPUT',val:event.target.value});
 
-    setFormIsValid(
-      emailState.isValid && event.target.value.trim().length > 6
-    );
-  };
+  //   setFormIsValid(
+  //     emailState.isValid && event.target.value.trim().length > 6
+  //   );
+  // };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    dispatchPassword({type:'USER_INPUT',val:event.target.value});
 
-    setFormIsValid(
-      emailState.value.includes('@') && event.target.value.trim().length > 6
-    );
+    // setFormIsValid(
+    //   emailState.value.includes('@') && event.target.value.trim().length > 6
+    // );
   };
 
   const validateEmailHandler = () => {
    dispatchEmail({type:'INPUT_BLUR',})
   };
-
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
-  };
+    dispatchPassword({type:'INPUT_BLUR',})
+   };
+ 
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -120,6 +138,6 @@ const Login = (props) => {
       </form>
     </Card>
   );
-};
+}}
 
 export default Login;
---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import classes from './Navigation.module.css';
+
+const Navigation = (props) => {
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
